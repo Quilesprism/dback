24a9ef34b5624cc4dddef63d5a245d56e51b5b6d
Quilesprism <quilesxasterin8@gmail.com>
2024-10-20 20:56:09 -0500

    add

--- cback/settings.py
+++ cback/settings.py
@@ -158,6 +158,9 @@ PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
     'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'https://dbback-1091848286124.us-central1.run.app',
